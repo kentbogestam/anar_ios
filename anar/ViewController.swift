0b962d2a81623017a47572383348c9fe22b869b6
Kent Bogestam <Kent.bogestam@cumbari.com>
2018-12-21 11:00:03 +0100

    Initial Commit

---
+++ anar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  anar
+//
+//  Created by Kent Bogestam on 2018-12-21.
+//  Copyright © 2018 Kent Bogestam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
