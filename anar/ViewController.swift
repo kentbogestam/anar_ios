6d4479b51cae693ae5ef9ecbf791108c45aa8092
Kent Bogestam <kentbogestam@Kents-niMac.local>
2019-06-05 15:21:45 +0200

    Last update from Ajit and added signature and icons and bundle id

--- anar/ViewController.swift
+++ anar/ViewController.swift
@@ -8,28 +8,137 @@
 
 import UIKit
 import WebKit
+import CoreLocation
 
-class ViewController: UIViewController, WKNavigationDelegate {
+class ViewController: UIViewController, WKNavigationDelegate, WKScriptMessageHandler, WKUIDelegate, CLLocationManagerDelegate {
     var webView: WKWebView!
+    let userContentController = WKUserContentController()
+    let locationManager = CLLocationManager()
+    var dict = ["lat": "0.00", "long": "0.00", "action": ""]
+    var positionA : CLLocation?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let url = URL(string: "https://anar.dastjar.com")!
+        let url = URL(string: "https://anar-dev.dastjar.com")!
+//        let url = URL(string: "http://localhost/demo/index.html")!
         webView.load(URLRequest(url: url))
-        
-        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
-        toolbarItems = [refresh]
-        navigationController?.isToolbarHidden = false
     }
-
+    
     override func loadView() {
-        webView = WKWebView()
+        super.loadView()
+        let config = WKWebViewConfiguration()
+        config.userContentController = userContentController
+        
+        webView = WKWebView(frame: self.view.bounds, configuration: config)
+        userContentController.add(self, name: "geoAddress")
         webView.navigationDelegate = self
+        webView.uiDelegate = self
         view = webView
     }
     
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        debugPrint(webView.url!)
+//        webView.evaluateJavaScript("alert('Hello from')", completionHandler: nil)
+        
+        
+    }
+    
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        title = webView.title
+        debugPrint("didFinish:\(webView.url!)")
+    }
+    
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+//        let dict = message.body as! [String:AnyObject]
+//        let username    = dict["username"] as! String
+//        let secretToken = dict["secretToken"] as! String
+        
+        if message.name == "geoAddress" {
+            let strMsg = message.body as! String
+            dict["action"] = strMsg
+            
+            let status = CLLocationManager.authorizationStatus()
+            
+            switch status {
+                
+            // 1
+            case .notDetermined:
+                locationManager.requestWhenInUseAuthorization()
+                return
+                
+            // 2
+            case .denied, .restricted:
+                let alert = UIAlertController(title: "Location Services disabled", message: "Please enable Location Services in Settings", preferredStyle: .alert)
+                let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+                alert.addAction(okAction)
+                
+                present(alert, animated: true, completion: nil)
+                return
+            case .authorizedAlways, .authorizedWhenInUse:
+                sendCurrentLocation(dict: self.dict as [NSString : NSString])
+                break
+                
+            }
+            
+            // 4
+            locationManager.delegate = self
+            locationManager.startUpdatingLocation()
+            
+        }
+        
+        // now use the name and token as you see fit!
+    }
+    
+    func webView(_ webView: WKWebView, runJavaScriptAlertPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping () -> Void) {
+        let alertController = UIAlertController(title: "", message: message, preferredStyle: .alert)
+        let otherAction = UIAlertAction(title: "OK", style: .default, handler: {
+            action in completionHandler()
+        })
+        alertController.addAction(otherAction)
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let lastLocation = locations.last!
+        debugPrint("didUpdateLocations")
+        dict = [
+            "lat": String(lastLocation.coordinate.latitude),
+            "long": String(lastLocation.coordinate.longitude)
+        ]
+        
+        if (positionA == nil) {
+            positionA = lastLocation
+        }
+        
+        let distanceInMeters = positionA?.distance(from: lastLocation)
+        print("distanceInMeters: \(distanceInMeters)")
+        
+        guard let distance = distanceInMeters else {
+            return
+        }
+        
+        if distance > 20 {
+            print("exceed 20 meter")
+            positionA = lastLocation
+            dict["action"] = "getLocation"
+            sendCurrentLocation(dict: self.dict as [NSString : NSString])
+        }
+        else {
+            print("under 20 meter")
+        }
+    }
+    
+    func sendCurrentLocation(dict: [NSString:NSString]) {
+        let jsonData = try! JSONSerialization.data(withJSONObject: dict, options: [])
+        let jsonString = String(data: jsonData, encoding: String.Encoding.utf8)!
+        print(jsonString)
+        // Send the location update to the page
+        self.webView.evaluateJavaScript("responseGeoAddressFromIosNative(\(jsonString))") { result, error in
+            guard error == nil else {
+                debugPrint(error ?? "")
+                return
+            }
+        }
     }
 
 }
